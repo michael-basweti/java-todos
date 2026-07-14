f297bb988bdc6a74ea7e8630a0919ca8e18db735
michael-basweti <baswetima@gmail.com>
2026-07-14 16:13:14 +0300

    collection table for authorities

--- src/main/java/com/mbasweti/springboot/todos/entity/User.java
+++ src/main/java/com/mbasweti/springboot/todos/entity/User.java
@@ -2,16 +2,21 @@ package com.mbasweti.springboot.todos.entity;
 
 import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.JoinColumn;
 
 @Table(name = "users")
 @Entity
@@ -41,6 +46,11 @@ public class User implements UserDetails{
     @Column(name = "updated_at")
     private Date updatedAt;
 
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_authorities", joinColumns = @JoinColumn(name = "user_id"))
+    private List<Authority> authorities;
+
     // private List<Todo> todos;
 
     // Default constructor
@@ -49,7 +59,12 @@ public class User implements UserDetails{
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return authorities;
+    }
+
+
+    public void setAuthorities(List<Authority> authorities) {
+        this.authorities = authorities;
     }
 
     @Override
